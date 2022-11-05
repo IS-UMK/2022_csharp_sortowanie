932ed8a42e7895c546099961a8905d9433713d5a
pk <pk@PK2-00>
2022-11-05 16:18:05 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SortowanieBabelkowe
+{
+    class Program
+    {
+        static string[] Sortuj(string[] tablica)
+        {
+            for (int k = 0; k < tablica.Length - 1; k++)
+            {
+                for (int i = 0; i < tablica.Length - 1; i++)
+                {
+                    if (tablica[i].CompareTo(tablica[i + 1]) > 0)
+                    {
+                        string x = tablica[i];
+                        tablica[i] = tablica[i + 1];
+                        tablica[i + 1] = x;
+                    }
+                }
+            }
+            return tablica;
+        }
+
+        static double[] Sortuj(double [] tablica)
+        {
+            for (int k = 0; k < tablica.Length - 1; k++)
+            {
+                for (int i = 0; i < tablica.Length - 1; i++)
+                {
+                    if (tablica[i] > tablica[i + 1])
+                    {
+                        double x = tablica[i];
+                        tablica[i] = tablica[i + 1];
+                        tablica[i + 1] = x;
+                    }
+                }
+            }
+            return tablica;
+        }
+
+        public static void Main()
+        {
+            var tablica = new double[]{ 10.0, 5.0, 3.0, 1.0 };
+            Sortuj(tablica);
+            foreach (var x in tablica)
+            {
+                Console.WriteLine(x);
+            }
+
+            var napisy = new string[] { "żółć", "złoto", "ala", "ąąąą"  };
+
+            Sortuj(napisy);
+            foreach (var x in napisy)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
